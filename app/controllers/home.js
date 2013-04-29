899698b1c30a430715e7708268d18aea7210bae3
Chris Langager <christopher.langager@gmail.com>
2013-04-29 19:13:26 -0400

    clean up

--- app/controllers/home.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (app) {
-    return {
-        index:[
-            function (req, res, next) {
-                //we just want to return an html file.  Angular will take care of the templating.
-                res.sendfile(app.dir + '/public/index.html');
-            }]
-    };
-};
