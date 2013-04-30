9419e94fe23531baf59474878cf3c71aeb5d76c1
Chris Langager <christopher.langager@gmail.com>
2013-04-29 20:24:26 -0400

    404 logic fixed

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
         index:[
             function (req, res, next) {
                 //we just want to return an html file.  Angular will take care of the templating.
-                res.sendfile(__dirname + '/../public/index.html');
+                res.sendfile(app.set('public') + '/index.html');
             }]
     };
 };
